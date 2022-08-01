e1798f3e8b65a9d29f0817f9d1f43639c6b522bf
Andrej <andrijvecirko2@gmail.com>
2022-08-01 11:26:51 +0300

    added cards of products

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,43 +1,43 @@
 let isMobile = {
-    Android: function () {
-        return navigator.userAgent.match(/Android/i);
-    },
-    BlackBerry: function () {
-        return navigator.userAgent.match(/BlackBerry/i);
-    },
-    iOS: function () {
-        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-    },
-    Opera: function () {
-        return navigator.userAgent.match(/Opera Mini/i);
-    },
-    Windows: function () {
-        return navigator.userAgent.match(/IEMobile/i);
-    },
-    any: function () {
-        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-    }
+	Android: function () {
+		return navigator.userAgent.match(/Android/i);
+	},
+	BlackBerry: function () {
+		return navigator.userAgent.match(/BlackBerry/i);
+	},
+	iOS: function () {
+		return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+	},
+	Opera: function () {
+		return navigator.userAgent.match(/Opera Mini/i);
+	},
+	Windows: function () {
+		return navigator.userAgent.match(/IEMobile/i);
+	},
+	any: function () {
+		return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+	}
 };
 
 let body = document.querySelector('body');
 if (isMobile.any()) {
-    body.classList.add('touch');
-    let arrow = document.querySelectorAll('.arrow');
-    for (let i = 0; i < arrow.length; i++) {
-        let thisLink = arrow[i].previousElementSibling;
-        let subMenu = arrow[i].nextElementSibling;
-        let thisArrow = arrow[i];
+	body.classList.add('touch');
+	let arrow = document.querySelectorAll('.arrow');
+	for (let i = 0; i < arrow.length; i++) {
+		let thisLink = arrow[i].previousElementSibling;
+		let subMenu = arrow[i].nextElementSibling;
+		let thisArrow = arrow[i];
 
-        console.log(thisLink, thisArrow);
+		console.log(thisLink, thisArrow);
 
-        thisLink.classList.add('parent');
-        arrow[i].addEventListener('click', function () {
-            subMenu.classList.toggle('open');
-            thisArrow.classList.toggle('active');
-        });
-    }
+		thisLink.classList.add('parent');
+		arrow[i].addEventListener('click', function () {
+			subMenu.classList.toggle('open');
+			thisArrow.classList.toggle('active');
+		});
+	}
 } else {
-    body.classList.add('mouse');
+	body.classList.add('mouse');
 }
 
 
@@ -47,27 +47,136 @@ console.log(menuLinks);
 
 const iconMenu = document.querySelector('.header__hamburger-menu');
 if (iconMenu) {
-    const menu = document.querySelector('.menu-header-body');
-    iconMenu.addEventListener('click', () => {
-        document.body.classList.toggle('_lock')
-        iconMenu.classList.toggle('_active');
-        menu.classList.toggle('_active');
-    })
+	const menu = document.querySelector('.menu-header-body');
+	iconMenu.addEventListener('click', () => {
+		document.body.classList.toggle('_lock')
+		iconMenu.classList.toggle('_active');
+		menu.classList.toggle('_active');
+	})
 }
 
 
 
 
 const slide = new Swiper('.attempt', {
-     pagination: { //навигация: булеты, текущее положение
-        el: '.swiper-pagination',
-        clickable: true, //
-      
-    }, 
-    keyboard: {
-        enabled: true,
-        onlyInViewport: true,
-    },
-    loop: true, 
+	pagination: { //навигация: булеты, текущее положение
+		el: '.swiper-pagination',
+		clickable: true, //
+
+	},
+	keyboard: {
+		enabled: true,
+		onlyInViewport: true,
+	},
+	loop: true,
 });
 
+
+//rating
+const ratings = document.querySelectorAll('.rating');
+if (ratings.length > 0) {
+	initRatings();
+}
+// Основная функция
+function initRatings() {
+	let ratingActive, ratingValue;
+	// "Бегаем" по всем рейтингам на странице
+	for (let index = 0; index < ratings.length; index++) {
+		const rating = ratings[index];
+		initRating(rating);
+	}
+	// Инициализируем конкретный рейтинг
+	function initRating(rating) {
+		initRatingVars(rating);
+
+		setRatingActiveWidth();
+
+		if (rating.classList.contains('rating_set')) {
+			setRating(rating);
+		}
+	}
+	// Инициализайция переменных
+	function initRatingVars(rating) {
+		ratingActive = rating.querySelector('.rating__active');
+		ratingValue = rating.querySelector('.rating__value');
+	}
+	// Изменяем ширину активных звезд
+	function setRatingActiveWidth(index = ratingValue.innerHTML) {
+		const ratingActiveWidth = index / 0.05;
+		ratingActive.style.width = `${ratingActiveWidth}%`;
+	}
+	// Возможность указать оценку 
+	function setRating(rating) {
+		const ratingItems = rating.querySelectorAll('.rating__item');
+		for (let index = 0; index < ratingItems.length; index++) {
+			const ratingItem = ratingItems[index];
+			ratingItem.addEventListener("mouseenter", function (e) {
+				// Обновление переменных
+				initRatingVars(rating);
+				// Обновление активных звезд
+				setRatingActiveWidth(ratingItem.value);
+			});
+			ratingItem.addEventListener("mouseleave", function (e) {
+				// Обновление активных звезд
+				setRatingActiveWidth();
+			});
+			ratingItem.addEventListener("click", function (e) {
+				// Обновление переменных
+				initRatingVars(rating);
+
+				if (rating.dataset.ajax) {
+					// "Отправить" на сервер
+					setRatingValue(ratingItem.value, rating);
+				} else {
+					// Отобразить указанную оцнку
+					ratingValue.innerHTML = index + 1;
+					setRatingActiveWidth();
+				}
+			});
+		}
+	}
+	async function setRatingValue(value, rating) {
+		if (!rating.classList.contains('rating_sending')) {
+			rating.classList.add('rating_sending');
+
+			// Отправика данных (value) на сервер
+			let response = await fetch('rating.json', {
+				method: 'GET',
+
+				//body: JSON.stringify({
+				//	userRating: value
+				//}),
+				//headers: {
+				//	'content-type': 'application/json'
+				//}
+
+			});
+			if (response.ok) {
+				const result = await response.json();
+
+				// Получаем новый рейтинг
+				const newRating = result.newRating;
+
+				// Вывод нового среднего результата
+				ratingValue.innerHTML = newRating;
+
+				// Обновление активных звезд
+				setRatingActiveWidth();
+
+				rating.classList.remove('rating_sending');
+			} else {
+				alert("Ошибка");
+
+				rating.classList.remove('rating_sending');
+			}
+		}
+	}
+}
+
+const markCardsBestseller = document.querySelectorAll('.mark-card__bestseller');
+
+markCardsBestseller.forEach(item => {
+	if(!item.previousElementSibling.classList.contains('mark-card__new')) {
+		item.classList.add('exist');
+	}
+});
